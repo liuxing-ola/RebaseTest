60d9a82d4c8f0527e67bd3aeec97534b2b57c7d5
liuxing <Liu.Xing@olaola.chat>
2024-03-04 14:31:53 +0800

    测试10

--- RebaseTest/AppDelegate.m
+++ RebaseTest/AppDelegate.m
@@ -19,6 +19,9 @@
     NSLog(@"测试5");
     NSLog(@"测试6");
     NSLog(@"测试7");
+    NSLog(@"测试8"); 
+    NSLog(@"测试9");
+    NSLog(@"测试10");
     // Override point for customization after application launch.
     return YES;
 }
