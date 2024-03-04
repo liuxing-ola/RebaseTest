29e1fb0999abf4559fd996b96fb196a45624723d
liuxing <Liu.Xing@olaola.chat>
2024-03-04 11:24:44 +0800

    测试5

--- RebaseTest/AppDelegate.m
+++ RebaseTest/AppDelegate.m
@@ -16,6 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"测试4");
+    NSLog(@"测试5");
     // Override point for customization after application launch.
     return YES;
 }
